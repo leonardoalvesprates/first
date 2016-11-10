84b6a048a9dec8031e57f6cea36dbeaf88d2c884
Leonardo Prates <leonardoalvesprates@gmail.com>
2016-11-10 20:04:24 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php
-echo $_SERVER['SERVER_ADDR'];
-?>
+ <?php 
+    $server = gethostname();
+    $nro = rand();
+    $alpha = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
+    $color = "#";
+    for ($i=0; $i<6; $i++) {
+        $index = rand(0,count($alpha)-1);
+        $color .= $alpha[$index];
+    }
+  $page = $_SERVER['PHP_SELF'];
+  $sec = "5";
+  echo "<p><h2>Hello World v28</h2></p>";
+  echo "<p>App Server:" . $server . "</p>";
+  echo "<p>Contador Aleatorio:" . $nro . "</p>";
+ ?>
+<html>
+ <head>
+  <title>My First Deploy PHP on OpenSHIFT</title>
+  <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
+ </head>
+ <body style="background:<?php echo $color ?>">
+ </body>
+</html>
