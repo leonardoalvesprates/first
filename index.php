07b96d8d4c1c40d39cf16fcab695a01d343f0d52
Leonardo Prates <leonardoalvesprates@gmail.com>
2018-10-30 16:02:24 -0300

    v2
    - removing background random colors and random number
    + adding PHP version

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
  <?php 
     $server = gethostname();
-    $nro = rand();
-    $alpha = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
-    $color = "#";
-    for ($i=0; $i<6; $i++) {
-        $index = rand(0,count($alpha)-1);
-        $color .= $alpha[$index];
-    }
+    // $nro = rand();
+    // $alpha = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
+    // $color = "#";
+    // for ($i=0; $i<6; $i++) {
+    //    $index = rand(0,count($alpha)-1);
+    //    $color .= $alpha[$index];
+    // }
   $page = $_SERVER['PHP_SELF'];
   $sec = "5";
-  echo "<p><h2>Hello World v28</h2></p>";
+  echo "<p><h2>Hello World v29 </h2></p>";
   echo "<p>App Server:" . $server . "</p>";
-  echo "<p>Contador Aleatorio:" . $nro . "</p>";
+  // echo "<p>Contador Aleatorio:" . $nro . "</p>";
+  echo 'PHP Version: ' . phpversion();
  ?>
 <html>
  <head>
   <title>My First Deploy PHP on OpenShiftt</title>
   <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
  </head>
- <body style="background:<?php echo $color ?>">
- </body>
+ // <body style="background:<?php echo $color ?>">
+ // </body>
 </html>
